cb04482610c292a7f8d4d045bd16b4351b4353d5
Marlon R. Pedrasita Jr <marlonpedrasita567@gmail.com>
2023-08-11 04:59:22 +0800

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // entry: "src/main.jsx",
+  entry: "src/main.jsx",
   base: "/react3/",
 });
